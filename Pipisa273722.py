a7a9b3545f5be5191515d4636876c2fabd4f5658
Hrrhhr <154808266+Hrrhhr@users.noreply.github.com>
2023-12-25 22:50:47 +0600

    Update Pipisa.py

--- Pipisa273722.py
+++ Pipisa273722.py
@@ -23,7 +23,8 @@ class Farmpipisaabot(loader.Module):
     async def b_run(self, client):
         while True:
             await client.send_message('@gfxprimechat', "/dick@pipisabot")
-            await asyncio.sleep(86400)
+            await client.send_message('@owlneverdiegroup', "/dick@pipisabot")
+            await asyncio.sleep(43200)
 
     @loader.unrestricted
     @loader.ratelimit
