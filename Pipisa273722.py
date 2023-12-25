7331409e12706f0757fc74b6f6f0803b30b6eff8
Hrrhhr <154808266+Hrrhhr@users.noreply.github.com>
2023-12-25 18:54:08 +0600

    Update Pipisa273722.py

--- Pipisa273722.py
+++ Pipisa273722.py
@@ -12,25 +12,49 @@ from .. import loader, utils
 
 
 @loader.tds
-class FarmPipisaBot(loader.Module):
-    """Модуль для автоматического фарминга в пиписьки в боте @pipisabot"""
+class FarmMonacoMod(loader.Module):
+    """Модуль для автоматического фарминга в игровом боте @pipisabot"""
 
-    strings = {"name": "pipisa"}
+    strings = {"name": "Pipisaaa"}
 
     def __init__(self):
         self.tasks = []
 
     async def b_run(self, client):
         while True:
-            await client.send_message('@pipisabot', "/dick@pipisabot")
+            await client.send_message('@pipisabot', "Ежедневный бонус")
             await asyncio.sleep(1200)
+
+    async def p_run(self, client):
+        while True:
+            await client.send_message('@pipisabot', "Бизнес снять")
+            await asyncio.sleep(1920)
+
+    async def l_run(self, client):
+        while True:
+            await client.send_message('@pipisabot', "Город снять")
+            await asyncio.sleep(1220)
+
+    async def t_run(self, client):
+        while True:
+            await client.send_message('@pipisabot', "Работать")
+            await asyncio.sleep(1440)
+
+    async def a_run(self, client):
+        while True:
+            await client.send_message('@pipisabot', "Ферма снять")
+            await asyncio.sleep(1680)
 
     @loader.unrestricted
     @loader.ratelimit
     async def farmcmd(self, message):
         """Запустить автоматический фарминг в боте"""
         if self.tasks:
-            return true
+            return await message.edit("Автоматический фарминг уже запущен.")
+        await message.edit("Автоматический фарминг запущен.")
+        client = message.client
+        self.tasks = [asyncio.create_task(self.b_run(client)), asyncio.create_task(self.p_run(client)), asyncio.create_task(self.l_run(client)), asyncio.create_task(self.t_run(client)), asyncio.create_task(self.a_run(client))]
+
     @loader.unrestricted
     @loader.ratelimit
     async def stopcmd(self, message):
